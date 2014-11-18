0cbfb9e2b0618122473cc3bde1c740affbfbaa6e
Hannu Siikonen <hannu.siikonen@aalto.fi>
2014-11-18 18:32:27 +0200

    minor changes to color scripts & progress in the ranking skripts

--- color/pcaplot.m
+++ color/pcaplot.m
@@ -1,25 +1,26 @@
 % This script is used to classify a wine to either red or white
 
 winefacts = readtable('../training_dataset.csv');
+winetests = readtable('../test_dataset.csv');
 
 %% Set convenient training and validation sets
 training = winefacts(1:5000,:);
 
-traing = [table2array(training(:,1:11)), strcmp(training.type, 'Red')];
+traing = [table2array(training(:,1:11)),training.quality,strcmp(training.type, 'Red')];
 
 %% PCA
-[vecs,coeffs] = pca(traing(:,1:11),'NumComponents',2);
+[vecs,coeffs] = pca(traing(:,1:11)./repmat(sqrt(var(traing(:,1:11))),5000,1),'NumComponents',2);
 
-pcapts = [traing(:, 1:11) * vecs, traing(:, 12)]
+%pcapts = [(traing(:, 1:11) * vecs, traing(:, 12)]
+pcapts = [coeffs, traing(:, end)];
 
-redc = ( pcapts(:, 3) == 1 )
-whitec = ( pcapts(:, 3) == 0 )
+red = pcapts(pcapts(:, end) == 1,:);
+white = pcapts(pcapts(:, end) == 0,:);
 
-red = pcapts(redc,:)
-white = pcapts(whitec,:)
-
-plot(red(:,1),red(:,2),'r+')
+plot(red(:,1),red(:,2),'r+');
 hold on
-plot(white(:,1),white(:,2),'y+')
+plot(white(:,1),white(:,2),'y+');
 
-print('-depsc','-r300','pca')
+%print('-depsc','-r300','pca')
+
+hold off
